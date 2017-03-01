371132f2557b49273829d1fcc3843498612879d1
minumn <taisen20@hotmail.com>
2017-03-01 18:36:44 +0100

    client and server updated to be able to send and save index.html

--- file_client_2.py
+++ file_client_2.py
@@ -7,20 +7,34 @@ HOST = 'localhost'
 BUFSIZE = 1000
 
 def main(argv):
-	fileName = argv
+	fileName = "/var/www/html/index.html"
+	#fileName = argv[1]
 	print '1 Filename: ', fileName	
 	clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	clientsocket.connect((HOST, PORT))
 	print '2 '
 	Lib.writeTextTCP(fileName, clientsocket)
 	print '3 '
-	recieveFile(fileName, clientsocket)
+	receiveFile(fileName, clientsocket)
 	print '5 ' 
+	receiveFile(fileName, clientsocket)
+	print '6 '
+	receiveFile(fileName, clientsocket)
+	print '7 '
+	receiveFile(fileName, clientsocket)
+	print '8 '
+	receiveFile(fileName, clientsocket)
+	print '9 '
 	clientsocket.close()
 	
 def receiveFile(fileName,  conn):
 	text = Lib.readTextTCP(conn)
-	print '4 Data received: ', text
+	fileName = Lib.extractFilename(fileName)
+	print '4 Data received: ', fileName
+	text_obj = open(fileName, "w")
+	text_obj.write(text)
+	print 'Text obj: ', text_obj
+	text_obj.close()
     
 
 if __name__ == "__main__":
--- file_server.py
+++ file_server.py
@@ -14,7 +14,9 @@ def main(argv):
 	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # INET equals streaming socket equals TCP
 	serversocket.bind((HOST,PORT)) 
 	serversocket.listen(1) # We want to queue up to just one client
+	#serversocket.setblocking(0)
 
+	#try:
 	while(1):
 		#Wait for connections
 		(clientsocket, address) = serversocket.accept()
@@ -26,55 +28,60 @@ def main(argv):
 		#filename = clientsocket.recv(BUFSIZE)	#Connect with telnet	
 		print '1 ', filename
 			#filename = exctractFilename(text)
-		#filesize = 0
+	
 		filesize = Lib.check_File_Exists(filename) # returns size
-		print '2 ', filesize
-
+		print '2 filesize ', filesize
+	
 		if filesize != 0:
 			sendFile(filename, filesize, clientsocket)
 		else: clientsocket.send("File " + filename + "doesnt exist. \0")
 	
 		print '3 '		
-
-		#clientsocket.close()
-		
-		
+	
+			#clientsocket.close()
+			
+			
 		print '4 '
 		data = "."
-
-		#while (1):
+	
+			#while (1):
 		for i in range(0,0):
 			data = clientsocket.recv(BUFSIZE)
 			clientsocket.send(data + "\0")
-#			if data == "quit": break
+			#if data == "quit": break
 			print data
 			i = i+1
-
-
+	
+	
 		#clientsocket.shutdown()
 		print 'Closing connection ', address
 		clientsocket.close()
 
+	#except socket.error, msg:
+	#	clientsocket.close()
+	#	serversocket.close()
 		
-
-
-
-
+			
+	
+	
+	
+	
 def sendFile(fileName,  fileSize,  conn):
 	# TO DO Your Code
-	length = BUFSIZE
-	for i in range(0, fileSize):
-		if i > fileSize - 1000:
-			i = fileSize - i
-		#writeTextTCP(fileName(i:i+length), conn)
-	#end
+	#length = BUFSIZE
+	#fileName = Lib.extractFilename(fileName)
+	file_obj = open(fileName, "r")
+	#print 'File to send: ', file_obj.read()
+	
+	Lib.writeTextTCP(file_obj.read(), conn)
+
 	print 'sendFile '
 	#conn.flush
 	
 
     
 if __name__ == "__main__":
-   main(sys.argv[1:])
+	main(sys.argv[1:])
 
 
 
