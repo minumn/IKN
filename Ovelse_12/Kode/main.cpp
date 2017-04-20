f7f389ef07eb3aac154f7dbb53ca52c38a1c4bd8
Tais <Taisen20@Hotmail.com>
2017-04-20 10:33:07 +0200

    added main.cpp to test link.send.

---
+++ Ovelse_12/Kode/main.cpp
@@ -0,0 +1,16 @@
+//Main.cpp til test af LINK laget
+
+#include <iostream>
+#include "link.h"
+#include <string>
+
+int main()
+{
+	string str = "hej derude."
+	Link link(length(str));
+	
+	link.send(str, length(str));
+	
+	
+	return 0;
+}
