62c1b6d71daf3508a47b47a169d291538640ad58
Jonas Lind <jonas.lind12@gmail.com>
2017-05-02 09:16:30 +0200

    Removed send

--- Ovelse_12/Kode/main.cpp
+++ Ovelse_12/Kode/main.cpp
@@ -1,7 +1,7 @@
 //Main.cpp til test af LINK laget
 
 #include <iostream>
-//#include "include/link.h"
+#include "include/link.h"
 #include <string>
 
 using namespace std;
@@ -18,44 +18,4 @@ int main()
 	
 	
 	return 0;
-}
-
-
-void ssend(const char buf[], short size)
-{
-	//TO DO Your own code 
-	int i, k = 0;
-	std::cout << "Incomming buf: " << buf << " with size : " << size << endl;
-
-	for (i = 0; i < size; i++)
-	{
-		buffer[k] = 'A';
-		k++;
-
-		if (buf[i] == 'A')
-		{
-			buffer[k] = 'B';
-			k++;
-			buffer[k] = 'C';
-			k++;
-		}
-		else if (buf[i] == 'B')
-		{
-			buffer[k] = 'B';
-			k++;
-			buffer[k] = 'D';
-			k++;
-		}
-		else
-		{
-			buffer[k] = buf[i];
-			k++;
-		}
-
-		buffer[k] = 'A';
-		k++;
-	}
-
-	std::cout << "Outgoing buffer: " << buffer << " with size: " << k << endl;
-
 }
