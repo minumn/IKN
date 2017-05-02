0b3458a3d0e2add4eadfa73f6c1e9d641e938434
Tais <Taisen20@Hotmail.com>
2017-05-02 08:52:09 +0200

    Updated.

--- Ovelse_12/Kode/main.cpp
+++ Ovelse_12/Kode/main.cpp
@@ -1,16 +1,61 @@
 //Main.cpp til test af LINK laget
 
 #include <iostream>
-#include "link.h"
+//#include "include/link.h"
 #include <string>
+
+using namespace std;
+
+void ssend(const char[], short);
+
+char* buffer = new char[(50 * 2)];
 
 int main()
 {
-	string str = "hej derude."
-	Link link(length(str));
-	
-	link.send(str, length(str));
+	const string str = "hej derude.";
+	const char *strr = &str;
+	ssend(strr, str.length);
 	
 	
 	return 0;
+}
+
+
+void ssend(const char buf[], short size)
+{
+	//TO DO Your own code 
+	int i, k = 0;
+	std::cout << "Incomming buf: " << buf << " with size : " << size << endl;
+
+	for (i = 0; i < size; i++)
+	{
+		buffer[k] = 'A';
+		k++;
+
+		if (buf[i] == 'A')
+		{
+			buffer[k] = 'B';
+			k++;
+			buffer[k] = 'C';
+			k++;
+		}
+		else if (buf[i] == 'B')
+		{
+			buffer[k] = 'B';
+			k++;
+			buffer[k] = 'D';
+			k++;
+		}
+		else
+		{
+			buffer[k] = buf[i];
+			k++;
+		}
+
+		buffer[k] = 'A';
+		k++;
+	}
+
+	std::cout << "Outgoing buffer: " << buffer << " with size: " << k << endl;
+
 }
