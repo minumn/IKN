6a2c87df5892d1c6fbef65cb5065275bf67806a9
Tais <Taisen20@Hotmail.com>
2017-05-11 22:59:29 +0200

    Copied functions from ex8.

--- Ovelse_12/Kode/Exercise_12_cpp_Client/file_client.cpp
+++ Ovelse_12/Kode/Exercise_12_cpp_Client/file_client.cpp
@@ -48,7 +48,44 @@ file_client::file_client(int argc, char **argv)
 /// </param>
 void file_client::receiveFile (std::string fileName, Transport::Transport *transport)
 {
-        // TO DO Your own code
+    // TO DO Your own code : Kopieret fra Ovelse 8
+	long fileSize = getFileSizeTCP(transport);
+	long recvSize = 0;
+	int len;
+	char buffer[BUFSIZE] = {0}, str[33] = {0};
+	ofstream outFile;
+
+	cout << "Fil størrelse: " << fileSize << "\n";
+
+	if(fileSize == 0)
+		cout << "Filen findes ikke på serveren\n";
+	else
+	{
+		printf("Filen overføres...\n");
+		outFile.open(extractFileName(fileName).c_str(),ios::out|ios::binary);
+
+        while ((long) recvSize < (long) fileSize)
+        {
+            len = read(transport, buffer, sizeof(buffer));
+            recvSize+= len;
+            outFile.write (buffer, len);
+            sprintf(str, "\rReceived: %6.1f%%", ((long)recvSize/1.0/(long) (fileSize)*100));
+            cout << str;
+        }
+
+        outFile.close();
+
+		if( recvSize == fileSize )
+		{
+		    cout << " OK\n";
+		}
+		else
+		{
+		    cout << " ERROR\n";
+		    remove(extractFileName(fileName).c_str());
+		}
+
+	}
 }		
 
 /// <summary>
@@ -60,6 +97,35 @@ void file_client::receiveFile (std::string fileName, Transport::Transport *trans
 int main(int argc, char** argv)
 {
 	new file_client(argc, argv);
+	
+	// Kopieret fra Ovelse 8
+	    int sockfd = 0;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    if (argc < 2)
+       error("FEJL: file_client <server ip-addresse> <[sti]filnavn>");
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("FEJL: Åbning af socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL)
+        error("FEJL: Ingen sådanne host");
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(PORT);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+        error("FEJL: Forbinder");
+
+    writeTextTCP(argv[2], sockfd);
+    receiveFile(argv[2], sockfd);
+
+    close(sockfd);
 	
 	return 0;
 }
