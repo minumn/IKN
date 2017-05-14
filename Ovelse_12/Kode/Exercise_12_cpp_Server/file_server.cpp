306c2061ec0479773d9dd489c8adb32309391c73
jonaslind10 <jonas.lind12@gmail.com>
2017-05-14 13:12:47 +0200

    Tester

--- Ovelse_12/Kode/Exercise_12_cpp_Server/file_server.cpp
+++ Ovelse_12/Kode/Exercise_12_cpp_Server/file_server.cpp
@@ -14,7 +14,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <lib.h>
+
 
 /// <summary>
 /// The BUFSIZE
@@ -49,37 +49,37 @@ file_server::file_server ()
 void file_server::sendFile(std::string fileName, long fileSize, Transport::Transport *transport)
 {
 	// To do Your own code : Kopieret fra Ovelse 8
-	char filesizeBuf[129] = {0};
-	char buffer[BUFSIZE] = {0};
-	int readBytes, sendBytes;
-	ifstream inFile;
-	ifstream::pos_type fposstart;
-	sprintf(filesizeBuf,"%li", fileSize);
-	send(transport, filesizeBuf, strlen(filesizeBuf)+1,0);
+//    char filesizeBuf[129] = {0};
+//    char buffer[BUFSIZE] = {0};
+//    int readBytes, sendBytes;
+//    ifstream inFile;
+//    ifstream::pos_type fposstart;
+//    sprintf(filesizeBuf,"%li", fileSize);
+//    send(transport, filesizeBuf, strlen(filesizeBuf)+1,0);
 
-	if(fileSize > 0)
-	{
-		inFile.open(fileName.c_str(), std::ios::in | std::ios::binary);
-		if(inFile.is_open())
-		{
-			std::cout << "Sending the file named " << fileName << "\n";
+//    if(fileSize > 0)
+//    {
+//        inFile.open(fileName.c_str(), std::ios::in | std::ios::binary);
+//        if(inFile.is_open())
+//        {
+//            std::cout << "Sending the file named " << fileName << "\n";
 
-			while(!inFile.eof())
-			{
-				fposstart = inFile.tellg();
-				inFile.read(buffer, sizeof(buffer));
-				readBytes = (int) ((long)inFile.tellg() == -1 ? fileSize : (long)inFile.tellg()) - fposstart;
-				sendBytes = send(transport, buffer, readBytes, 0);
+//            while(!inFile.eof())
+//            {
+//                fposstart = inFile.tellg();
+//                inFile.read(buffer, sizeof(buffer));
+//                readBytes = (int) ((long)inFile.tellg() == -1 ? fileSize : (long)inFile.tellg()) - fposstart;
+//                sendBytes = send(transport, buffer, readBytes, 0);
 
-				if(readBytes != sendBytes)
-					break;
-			}
-		}
-	}
-	else
-		std::cout << "The file " << fileName << " not found\n";
+//                if(readBytes != sendBytes)
+//                    break;
+//            }
+//        }
+//    }
+//    else
+//        std::cout << "The file " << fileName << " not found\n";
 
-	std::cout << "Finish file sending...\n";
+//    std::cout << "Finish file sending...\n";
 }
 
 /// <summary>
@@ -92,46 +92,46 @@ int main(int argc, char **argv)
 {
 	new file_server();
 	
-	// Kopieret fra Ovelse 8
-	int welcomSocketfd, clientSocketfd, pos;
-    struct sockaddr_in serv_addr, cli_addr;
-    string fileName = "";
-    char ch;
-    long fileSize;
+//    // Kopieret fra Ovelse 8
+//    int welcomSocketfd, clientSocketfd, pos;
+//    struct sockaddr_in serv_addr, cli_addr;
+//    string fileName = "";
+//    char ch;
+//    long fileSize;
 
-    welcomSocketfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (welcomSocketfd < 0)
-       error("ERROR opening socket");
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(PORT);
-    if (bind(welcomSocketfd, (struct sockaddr *) &serv_addr,
-             sizeof(serv_addr)) < 0)
-             error("ERROR on binding");
-    listen(welcomSocketfd,1);
+//    welcomSocketfd = socket(AF_INET, SOCK_STREAM, 0);
+//    if (welcomSocketfd < 0)
+//        error("ERROR opening socket");
+//    bzero((char *) &serv_addr, sizeof(serv_addr));
+//    serv_addr.sin_family = AF_INET;
+//    serv_addr.sin_addr.s_addr = INADDR_ANY;
+//    serv_addr.sin_port = htons(PORT);
+//    if (bind(welcomSocketfd, (struct sockaddr *) &serv_addr,
+//             sizeof(serv_addr)) < 0)
+//        error("ERROR on binding");
+//    listen(welcomSocketfd,1);
 
-    while(1)
-    {
-		fileName = "";
-		cout << "Server waiting...\n";
-		clientSocketfd = -1;
-		while(clientSocketfd == -1)
-		{
-			clientSocketfd = accept(welcomSocketfd,NULL, NULL);
-		}
-		cout << "Client connected.\n";
-    	if (clientSocketfd < 0)
-    		error("ERROR on accept");
+//    while(1)
+//    {
+//        fileName = "";
+//        cout << "Server waiting...\n";
+//        clientSocketfd = -1;
+//        while(clientSocketfd == -1)
+//        {
+//            clientSocketfd = accept(welcomSocketfd,NULL, NULL);
+//        }
+//        cout << "Client connected.\n";
+//        if (clientSocketfd < 0)
+//            error("ERROR on accept");
 
-		fileName = readTextTCP(fileName, clientSocketfd);
-			
-    	cout << "Searching for " << fileName << endl;
-    	fileSize = check_File_Exists(fileName);
-    	sendFile(fileName, fileSize, clientSocketfd);
-    	close(clientSocketfd);
-    }
-	return EXIT_SUCCESS;
+//        fileName = readTextTCP(fileName, clientSocketfd);
+
+//        cout << "Searching for " << fileName << endl;
+//        fileSize = check_File_Exists(fileName);
+//        sendFile(fileName, fileSize, clientSocketfd);
+//        close(clientSocketfd);
+//    }
+//    return EXIT_SUCCESS;
 	
 	return 0;
 }
